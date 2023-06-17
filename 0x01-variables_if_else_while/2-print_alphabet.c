7c59618beaed9b62392750abb29b60b06a3c5a1b
Umurhurhu <umurhurhuemmanuella5@gmail.com>
2023-06-17 15:26:55 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabets in lower case
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
