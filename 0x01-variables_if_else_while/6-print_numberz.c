b4b8444e88887beb5e6b9e60e7d6a29552965cdc
Umurhurhu <umurhurhuemmanuella5@gmail.com>
2023-06-19 17:52:17 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int z;
+
+	for (z = 48; z < 58; z++)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
