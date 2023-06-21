8b2e21e59a9b1ce15b07083a3f1e6fb838f80e23
Umurhurhu <umurhurhuemmanuella5@gmail.com>
2023-06-21 14:54:26 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic characters
+ * @c: The character to print
+ * Return: 1 if true, otherwise return 0
+ */
+int _isalpha(int c)
+{
+	char i, d;
+
+	if (i >= 'a' && i <= 'z')
+	{
+		return (1);
+	}
+	{
+		if (d >= 'A' && <= 'Z')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
