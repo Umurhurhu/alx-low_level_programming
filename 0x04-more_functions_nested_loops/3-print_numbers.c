edeac86dc5274621cc074e7a4b30758497e26ba3
Umurhurhu <umurhurhuemmanuella5@gmail.com>
2023-06-23 07:40:34 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ * Return: 0 (success)
+ */
+void print_numbers(void)
+{
+	char c;
 
+	for (c = '0'; c <= 9;  c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+}
