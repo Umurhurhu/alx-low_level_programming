394ef4cc4e638092ab3fc1f07429b5c21f062801
Umurhurhu <Umurhurhu@gmail.com>
2023-07-27 22:54:00 +0100

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void __attribute__((constructor)) hare(void);
+
+/**
+ * hare - Prints a string before the
+ * main function is executed.
+ */
+void hare(void)
+{
+printf("Hello, Holberton\n");
+}
